4ea2c6cd93bdae73e65ae1a93dafeb6b3691f141
Bryan Martins <bryan.araujo@curseduca.com>
2024-05-24 08:28:11 -0300

    ensure AxiosHttpclient rethrows if axios throws

--- tests/infra/http/axios-client.spec.ts
+++ tests/infra/http/axios-client.spec.ts
@@ -52,5 +52,16 @@ describe("AxiosHttpClient", () => {
 
             expect(result).toEqual("any_data");
         });
+
+        it("should rethrow if get throws", async () => {
+            fakeAxios.get.mockRejectedValueOnce(new Error("http_error"));
+
+            const promise = sut.get({
+                url,
+                params,
+            });
+
+            expect(promise).rejects.toThrow(new Error("http_error"));
+        });
     });
 });

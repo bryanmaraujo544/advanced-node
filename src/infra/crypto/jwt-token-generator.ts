a673fb233ad405606959101453c780403b8c31c6
Bryan Martins <bryan.araujo@curseduca.com>
2024-05-24 08:51:09 -0300

    moving implementation class of tokenGenerator to production folder

---
+++ src/infra/crypto/jwt-token-generator.ts
@@ -0,0 +1,17 @@
+import { TokenGenerator } from "@/data/contracts/crypto";
+import jwt from "jsonwebtoken";
+
+export class JwtTokenGenerator {
+    constructor(private readonly secret: string) {}
+
+    async generateToken(
+        params: TokenGenerator.Params
+    ): Promise<TokenGenerator.Result> {
+        const expirationInSeconds = params.expirationInMs / 1000;
+
+        const token = jwt.sign({ key: params.key }, this.secret, {
+            expiresIn: expirationInSeconds,
+        });
+        return token;
+    }
+}

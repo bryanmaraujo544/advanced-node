9e08114dfbda4005dce132f4ceca32596f99b9bf
Bryan Martins <bryan.araujo@curseduca.com>
2024-05-29 07:51:51 -0300

    chore: sharing sut creation to all methods of user-account repo

--- tests/infra/postgres/repos/user-account.spec.ts
+++ tests/infra/postgres/repos/user-account.spec.ts
@@ -5,24 +5,24 @@ import { getRepository, Repository, getConnection } from "typeorm";
 import { makeFakeDb } from "@/tests/infra/postgres/mocks";
 
 describe("PgUserAccountRepository", () => {
+    let sut: PgUserAccountRepository;
+    let pgUserRepo: Repository<PgUser>;
+    let backup: IBackup;
+
+    beforeAll(async () => {
+        const db = await makeFakeDb();
+        backup = db.backup();
+        pgUserRepo = getRepository(PgUser);
+    });
+    afterAll(async () => {
+        await getConnection().close();
+    });
+    beforeEach(() => {
+        backup.restore();
+        sut = new PgUserAccountRepository();
+    });
+
     describe("load", () => {
-        let sut: PgUserAccountRepository;
-        let pgUserRepo: Repository<PgUser>;
-        let backup: IBackup;
-
-        beforeAll(async () => {
-            const db = await makeFakeDb();
-            backup = db.backup();
-            pgUserRepo = getRepository(PgUser);
-        });
-        afterAll(async () => {
-            await getConnection().close();
-        });
-        beforeEach(() => {
-            backup.restore();
-            sut = new PgUserAccountRepository();
-        });
-
         it("should return an account if email exists", async () => {
             await pgUserRepo.save({ email: "existing_email" });
 

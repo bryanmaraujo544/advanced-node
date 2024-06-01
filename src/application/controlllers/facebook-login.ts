f1ef8173b9ef4133f02db7d24249379515e73664
Bryan Martins <bryan.araujo@curseduca.com>
2024-06-01 10:37:16 -0300

    chore: creating ok helper

--- src/application/controlllers/facebook-login.ts
+++ src/application/controlllers/facebook-login.ts
@@ -1,6 +1,12 @@
 import { FacebookAuthentication } from "@/domain/features";
 import { RequiredFieldError } from "../errors";
-import { HttpResponse, badRequest, serverError, unathorized } from "../helpers";
+import {
+    HttpResponse,
+    badRequest,
+    ok,
+    serverError,
+    unathorized,
+} from "../helpers";
 import { AuthenticationError } from "@/domain/errors";
 
 export class FacebookLoginController {
@@ -22,12 +28,9 @@ export class FacebookLoginController {
                 return unathorized();
             }
 
-            return {
-                statusCode: 200,
-                data: {
-                    accessToken: accessToken.value,
-                },
-            };
+            return ok({
+                accessToken: accessToken.value,
+            });
         } catch (err: any) {
             return serverError(err);
         }

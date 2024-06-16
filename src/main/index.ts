034db3adb6a3c16bbaa6006cced9107eed98e701
Bryan Martins <bryan.araujo@curseduca.com>
2024-06-16 18:03:28 -0300

    chore: orm config

--- src/main/index.ts
+++ src/main/index.ts
@@ -3,9 +3,8 @@ import "reflect-metadata";
 import { app } from "./config/app";
 import { env } from "@/main/config/env";
 import { createConnection } from "typeorm";
-import { config } from "@/infra/postgres/helpers/config";
 
-createConnection(config)
+createConnection()
     .then(() => {
         app.listen(env.port, () =>
             console.log(`Server is running at http://localhost:${env.port}`)

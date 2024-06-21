a1eb5094907dc41914fbd94c6ab5dda19b6233aa
Bryan Martins <bryan.araujo@curseduca.com>
2024-06-21 08:18:10 -0300

    test: ensure AuthenticationMiddleware returns 403 if authorize throws

--- tests/application/middlewares/authentication.spec.ts
+++ tests/application/middlewares/authentication.spec.ts
@@ -10,14 +10,18 @@ class AuthenticationMiddleware {
     async handle({
         authorization,
     }: HttpRequest): Promise<HttpResponse<Error> | undefined> {
-        const error = new RequiredStringValidator(
-            authorization,
-            "authorization"
-        ).validate();
-        if (error) {
+        try {
+            const error = new RequiredStringValidator(
+                authorization,
+                "authorization"
+            ).validate();
+            if (error) {
+                return forbidden();
+            }
+            await this.authorize({ token: authorization });
+        } catch {
             return forbidden();
         }
-        await this.authorize({ token: authorization });
     }
 }
 
@@ -58,5 +62,16 @@ describe("AuthenticationMiddleware", () => {
 
         expect(authorize).toHaveBeenCalledWith({ token: authorization });
         expect(authorize).toHaveBeenCalledTimes(1);
+    });
+
+    it("should return 403 if authorize throws", async () => {
+        authorize.mockRejectedValueOnce(new Error("any_error"));
+
+        const httpResponse = await sut.handle({ authorization });
+
+        expect(httpResponse).toEqual({
+            statusCode: 403,
+            data: new ForbiddenError(),
+        });
     });
 });

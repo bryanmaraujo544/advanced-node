694d6db9144bfc828955c34f6d4824d354ec1a69
Bryan Martins <bryan.araujo@curseduca.com>
2024-06-26 21:38:52 -0300

    chore: uuid handler returns

--- tests/infra/crypto/uuid-handler.spec.ts
+++ tests/infra/crypto/uuid-handler.spec.ts
@@ -1,11 +1,12 @@
 import { UUIDGenerator } from "@/domain/contracts/gateways/uuid";
+import { mocked } from "jest-mock";
 import { v4 } from "uuid";
 
 jest.mock("uuid");
 
 export class UUIDHandler implements UUIDGenerator {
     uuid({ key }: UUIDGenerator.Input): string {
-        return v4();
+        return `${key}_${v4()}`;
     }
 }
 describe("UUIDHandler", () => {
@@ -14,5 +15,13 @@ describe("UUIDHandler", () => {
 
         sut.uuid({ key: "any_key" });
         expect(v4).toHaveBeenCalledTimes(1);
+    });
+
+    it("should return correct uuid", () => {
+        mocked(v4).mockReturnValueOnce("1");
+        const sut = new UUIDHandler();
+
+        const uuid = sut.uuid({ key: "any_key" });
+        expect(uuid).toBe(`any_key_1`);
     });
 });

ae08d4c8bc3544675eb9f7cb2fdcf10fe5c9fbdf
Bryan Martins <bryan.araujo@curseduca.com>
2024-08-22 21:16:18 -0300

    chore: allowed mime Types undefined

--- tests/application/validations/allowed-mime-types.spec.ts
+++ tests/application/validations/allowed-mime-types.spec.ts
@@ -8,16 +8,26 @@ class AllowedMimeTypes {
         private readonly value: string
     ) {}
 
-    validate(): Error {
-        return new InvalidMimeTypeError(this.allowed);
+    validate(): Error | undefined {
+        if (this.allowed.includes("png") && this.value !== "image/png") {
+            return new InvalidMimeTypeError(this.allowed);
+        }
+        return undefined;
     }
 }
 
 describe("AllowedMimeTypes", () => {
     it("should return InvalidMimeTypeError if value is invalid", () => {
-        const sut = new AllowedMimeTypes(["png"], "image/png");
+        const sut = new AllowedMimeTypes(["png"], "image/jpg");
 
         const error = sut.validate();
         expect(error).toEqual(new InvalidMimeTypeError(["png"]));
+    });
+
+    it("should return undefined if value is valid", () => {
+        const sut = new AllowedMimeTypes(["png"], "image/png");
+
+        const error = sut.validate();
+        expect(error).toBeUndefined();
     });
 });
